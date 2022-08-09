fa8e4d5e066a3e375b41ac2a903bdf6e195ebf77
samuelxu999 <samuelxu999@gmail.com>
2022-08-09 10:15:26 -0400

    Update setup_localtest.sh.

--- script/setup_localtest.sh
+++ script/setup_localtest.sh
@@ -1,38 +1,49 @@
 #!/bin/bash
 
-# new folder based on test node size
-for i in 1 2 3 4
+## variable
+localtest_folder="../local_test"
+src_folder="../src"
+config_folder="../config"
+
+## new folder based on test node size
+for i in 1 2 3 4 5
 do
 	# new folders for local test
-	if [ ! -d "local_test$i" ]
+	if [ ! -d "$localtest_folder$i" ]
 	then
-		mkdir local_test$i
+		mkdir $localtest_folder$i
 	fi
 
-	# copy server and client code to localtest folder
-	cp ./WS_Server.py local_test$i/
-	cp ./WS_Client.py local_test$i/
+	## copy server and client code to localtest folder
+	cp $src_folder/WS_Server.py $localtest_folder$i/
+	cp $src_folder/WS_Client.py $localtest_folder$i/
 
-	# copy libs to localtest folder
-	cp -r ./cryptolib local_test$i/
-	cp -r ./network local_test$i/
-	cp -r ./consensus local_test$i/
-	cp -r ./randomness local_test$i/
-	cp -r ./utils local_test$i/
-	cp -r ./kademlia local_test$i/
-	cp -r ./rpcudp local_test$i/
+	## copy libs to localtest folder
+	cp -r $src_folder/cryptolib $localtest_folder$i/
+	cp -r $src_folder/network $localtest_folder$i/
+	cp -r $src_folder/consensus $localtest_folder$i/
+	cp -r $src_folder/randomness $localtest_folder$i/
+	cp -r $src_folder/utils $localtest_folder$i/
+	cp -r $src_folder/kademlia $localtest_folder$i/
+	cp -r $src_folder/rpcudp $localtest_folder$i/
 
-	# clear test data and results
-	rm -rf local_test$i/chaindata/*
-	rm -rf local_test$i/randomdata/*
-	rm -rf local_test$i/test_results/*
-	rm -f local_test$i/nodedata/*
+	## copy data
+	# cp -r $src_folder/data $localtest_folder$i/
+
+	## copy swarm_server.json
+	# cp $config_folder/swarm_server.json $localtest_folder$i/
+
+	## clear test data and results
+	rm -rf $localtest_folder$i/chaindata/*
+	rm -rf $localtest_folder$i/randomdata/*
+	rm -rf $localtest_folder$i/test_results/*
+	rm -rf $localtest_folder$i/nodedata/*
 
 done
 
-## clear test data and results
-rm -rf ./chaindata/*
-rm -rf ./randomdata/*
-rm -rf ./test_results/*
-rm -f ./nodedata/*
-touch ./WS_Server.py
+# ## clear test data and results
+# rm -rf ./chaindata/*
+# rm -rf ./randomdata/*
+# rm -rf ./test_results/*
+# rm -f ./nodedata/*
+# touch ./WS_Server.py
